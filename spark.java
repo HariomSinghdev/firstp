e69aa27a0be5fec332adb06043e86b3d074132ed
Hariom Singh <hariomsingh7004@gmail.com>
2025-02-25 23:50:21 +0530

    file changed

--- spark.java
+++ spark.java
@@ -2,5 +2,8 @@ class spark{
     public static void main(String[] args) {
         System.out.println("hello");
     }
+    public void add(){
+        System.out.println("we are addding");
+    }
 
 }
