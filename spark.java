e43abf99c6b5c2514cb1b90856a75ceb671e068f
Hariom Singh <hariomsingh70044@gmail.com>
2025-02-26 10:48:56 +0530

    heloo to me

--- spark.java
+++ spark.java
@@ -1,6 +1,6 @@
 class spark{
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello to me");
     }
     public void add(){
         System.out.println("we are addding notificatiog");
